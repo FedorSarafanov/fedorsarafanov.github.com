c613ec5cd1e8e8c3291d48067108593763c8aacb
FedorSarafanov <sfg180@yandex.ru>
2017-01-16 21:30:16 +0300

    Переделан gulpfile, упрощен/переделаны watch, добавлены скрипты сохранения/восстановления mtime.

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,6 @@
-var gulp = require('gulp'),  
-	refresh         = require('gulp-livereload'),   // Перезагрузка при вызове refresh(nameserver)
-	webserver       = require('gulp-webserver'),    // Сервер локалхоста
-	concat          = require('gulp-concat'),       // Объединяет файлы в один
-	minifyCSS       = require('gulp-minify-css'),   // Сжимает, оптимизирует
-	less            = require('gulp-less'),         // Less
-	// rigger          = require('gulp-rigger'),       // Замена в файлах    
-	rename          = require("gulp-rename");       // Переименование
-	autoprefixer    = require('less-plugin-autoprefix'), // Автопрефиксер свойств для старых браузеров
-    autoprefix      = new autoprefixer({browsers: ["last 3 versions"]}), //Свойства для браузеров от 2010 года	
-    dom             = require('gulp-dom'),          // Работа с распарсенным html
-    cache           = require('gulp-cached'),       // Обработка только modified файлов
-    runSequence     = require('run-sequence');      // Последовательные task-и
-    sort            = require('gulp-sort'),         // Сортировка файлов в gulp.src
-    paths           = require('path'),              // Получение basename         
-    util            = require('util'),              // .inspect (для получения даты)
-    fs              = require('fs'),                // Работа с файловой системой
-    markdown        = require('gulp-markdown'),
-    jsonfile        = require('jsonfile')          // Работа c json
-    , mdEqs         = require('gulp-markdown-equations')
-    , tap           = require('gulp-tap')
-    , filter        = require('gulp-filter')
-    , latex         = require('gulp-latex')
-    , pdftocairo    = require('gulp-pdftocairo'),
-    fileinclude = require('gulp-file-include')
 var config = {
     path: {
-        build: { //Тут мы укажем куда складывать готовые после сборки файлы
+        build: { 
             html: './',
             js: 'js/',
             css: 'css/',
@@ -38,30 +13,36 @@ var config = {
             fonts: 'font/',
             article: 'article/'
         },
-        sync:{ //Синхронизируемые папки
+        sync:{ 
             sprites: '/src/img/sprites/',
             problems: '/src/img/problems/',
             screenshots: '/src/img/screenshots/',
             fonts: '/src/font/'
         },
-        src: { //Пути откуда брать исходники
+
+        src: { 
             html: 'src/*.html',
             index: 'src/index.html',
+            problems: 'src/problems.html',
             js: 'src/js/main.js',
             style: 'src/less/*.less',
             fonts: 'src/font/**/*.*',
-            article: ['src/article/**/*.html','src/article/**/*.md'],
-            // article: 'src/article/**/*.mdtex', //['src/article/**/*.html','src/article/**/*.md'],
+            article: 'src/article/*.html',
             template: 'src/template/'
         },
-        watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        watch: {
             html: 'src/*.html',
-            article: ['src/article/**/*.html','src/article/**/*.md'],
-            // article: 'src/article/**/*.mdtex',//['src/article/**/*.html','src/article/**/*.md'],
+            article: 'src/article/*.html',
             js: 'src/js/**/*.js',
             style: 'src/less/**/*.less',
             fonts: 'src/fonts/**/*.*',
-            template: 'src/template/*.html'
+            template: 'src/template/*.html',
+            templates: {
+                layout: ['src/template/footer.html', 'src/template/head.html', 'src/template/header.html'],
+                article: ['src/template/article.html'],
+                index: ['src/template/articles.html'],
+                problems: ['src/template/irodov.html', 'src/template/wrote.html', 'src/template/yakovlev.html']
+            }
         }
     },
     date: {
@@ -78,209 +59,194 @@ var config = {
         linux: 'linux.html'
     },
     server: {
-      livereload: true,
-      directoryListing: false,
-      port: 5671,
-      open: false
+        livereload: true,
+        port: 5671
     },
     templates: {
         article: "<header><time class='datetime'>{date}</time><h1>{href}</h1></header><article>{text}{readmore}</article>"
+    },
+    replace: {
+        prefix: '@@',
+        asepath: '@file'
     }
 }
 
-jsonfile.readFile(config.files.cache, function(err, obj) {
-  cache.caches=obj;
+function build(file,type){
+    var filePath    =   paths.basename(file.path);
+    var stats       =   fs.statSync(file.path);
+    var date        =   new Date(util.inspect(stats.mtime)).toLocaleString('en-US',config.date.options);
+    var contents    =   String(file.contents);
+
+    var title       =   String(/<h1>(.|\n)*<\/h1>/igm.exec(contents)[0])
+                                .replace('<h1>','')
+                                .replace('</h1>','');
+        
+    var text        =   /<!--d-->(.|\n)*/igm.exec(contents)[0];
+    var desc        =   /<!--d-->(.|\n)*<!--ed-->/igm.exec(contents)[0];
+
+    var href        =   "<a href='{0}'>{1}</a>"
+                        .replace('{0}',config.path.build.article+filePath)
+                        .replace('{1}',title);
+
+    var href2       =   "<a class='readmore' href='{0}'>{1}</a>"
+                        .replace('{0}',config.path.build.article+filePath)
+                        .replace('{1}','Читать далее...');
+
+    if (type==="desc") {
+        contents = String(config.templates.article)
+                        .replace('{readmore}',href2)
+                        .replace('{date}',date)
+                        .replace('{href}',href)
+                        .replace('{text}',desc);
+    }
+
+    if (type==="article") {
+        contents = String(articleTemplate)
+                        .replace('{title}',title)
+                        .replace('{section}',config.templates.article)
+                        .replace('{readmore}','')
+                        .replace('{date}',date)
+                        .replace('{href}',title)
+                        .replace('{text}',text);
+    }
+
+    file.contents = new Buffer(contents);
+    return file;
+}
+
+function compare(file1,file2){
+    var stats1 = fs.statSync(file1.path),
+        stats2 = fs.statSync(file2.path);
+    var time1 = new Date(util.inspect(stats1.mtime)),
+        time2 = new Date(util.inspect(stats2.mtime));
+    if (time1<time2) {
+        return 1;
+    }
+    if (time1>time2) { 
+        return -1;
+    }
+    return 0;
+}
+
+var gulp            = require('gulp'),  
+    
+    less            = require('gulp-less'),         // Less
+    minifyCSS       = require('gulp-minify-css'),   // Сжимает, оптимизирует
+    autoprefixer    = require('less-plugin-autoprefix'), // Автопрефиксер свойств для старых браузеров
+    autoprefix      = new autoprefixer({browsers: ["last 3 versions"]}), //Свойства для браузеров от 2010 года  
+    
+    cache           = require('gulp-cached'),       // Обработка только modified файлов
+    sort            = require('gulp-sort'),         // Сортировка файлов в gulp.src
+    rename          = require("gulp-rename");       // Переименование
+    concat          = require('gulp-concat'),       // Объединяет файлы в один
+    
+    fs              = require('fs'),                // Работа с файловой системой
+    paths           = require('path'),              // Получение basename         
+    util            = require('util'),              // .inspect (для получения даты)
+    jsonfile        = require('jsonfile')           // Работа c json
+    
+    fileinclude     = require('gulp-file-include'),
+    markdown        = require('gulp-markdown'),
+    tap             = require('gulp-tap'),
+    
+    argv            = require('yargs').argv,
+    connect         = require('gulp-connect'),
+    runSequence     = require('run-sequence'),
+    spawn           = require('child_process').spawn, 
+
+    articleTemplate = fs.readFileSync(config.path.src.template+'article.html', 'utf8');
+
+gulp.task('default', ['server','auto-reload','watch'], function () {
+    jsonfile.readFile(config.files.cache, function(err, obj) {
+        cache.caches=obj;
+    })
 })
 
-gulp.task('article:rebase', ['article:compile','article:compile-bc'], function() {
-    return gulp.src(config.path.src.article)
-        .pipe(sort({// Сортировка: сначала новые
-                comparator: function(file1, file2) {
-                    var stats1 = fs.statSync(file1.path),
-                        stats2 = fs.statSync(file2.path);
-                    var time1 = new Date(util.inspect(stats1.mtime)),
-                        time2 = new Date(util.inspect(stats2.mtime));
-                    if (time1<time2) {
-                        return 1;
-                    }
-                    if (time1>time2) { 
-                        return -1;
-                    }
-                    return 0;
-                }
-        }))
-        .pipe(markdown())
-        .on('data', function(file) {
-            var filePath    =   paths.basename(file.path);
-            // console.log(file.path)
-            var stats       =   fs.statSync(file.path);
-            var date        =   new Date(util.inspect(stats.mtime)).toLocaleString('en-US',config.date.options);
-            var contents    =   String(file.contents);
-            var title       =   /<h1>(.|\n)*<\/h1>/igm.exec(contents)[0];
-                title       =   title.replace('<h1>','').replace('</h1>','');
-            var desc        =   /<!--d-->(.|\n)*<!--ed-->/igm.exec(contents)[0];
-            var href        =   "<a href='{0}'>{1}</a>"
-                                .replace('{0}',config.path.build.article+filePath)
-                                .replace('{1}',title);
-            var href2        =   "<a class='readmore' href='{0}'>{1}</a>"
-                                .replace('{0}',config.path.build.article+filePath)
-                                .replace('{1}','Читать далее...');
-            contents        =   config.templates.article;
-
-            contents        =   contents.replace('{readmore}',href2);
-            contents        =   contents.replace('{date}',date).replace('{href}',href).replace('{text}',desc);
-            file.contents = new Buffer(contents);
-            return file;
-        }) 
-        .pipe(concat(config.files.articles))
-        .pipe(gulp.dest(config.path.src.template))
-});
-
-
-gulp.task('article:compile', function() {
-    return gulp.src(config.path.src.article)
-        .pipe(cache('article'))
-        .pipe(markdown())
-        .pipe(fileinclude({
-          prefix: '@@',
-          basepath: '@file'
-        }))
-        .on('data', function(file) {
-            jsonfile.writeFile(config.files.cache, cache.caches, function (err) {})
-
-            var filePath    =   paths.basename(file.path);
-            // console.log(file.path)
-            var stats       =   fs.statSync(file.path);
-            var date        =   new Date(util.inspect(stats.mtime)).toLocaleString('en-US',config.date.options);
-            var contents    =   String(file.contents);
-            var title       =   /<h1>(.|\n)*<\/h1>/igm.exec(contents)[0];
-                title       =   title.replace('<h1>','').replace('</h1>','');
-
-            var tag         =   /<!--tags:(.*)-->/igm.exec(contents)[1];
-            var text        =   /<!--d-->(.|\n)*/igm.exec(contents)[0];
-            var href        =   "<a href='{0}'>{1}</a>"
-                                .replace('{0}',filePath)
-                                .replace('{1}',title);
-            contents        =   config.templates.article;
-            contents        =   contents.replace('{date}',date).replace('{href}',href).replace('{text}',text);
-            var html        =   fs.readFileSync(config.path.src.template+'article.html', 'utf8');
-            html            =   html.replace('{title}',title).replace('{text}',contents);
-            file.contents   =   new Buffer(html);
-            return file;
-        })
-        .pipe(fileinclude({
-          prefix: '@@',
-          basepath: '@file'
-        }))
-        .pipe(gulp.dest(config.path.build.article));
-});
-
-
-gulp.task('article:compile-bc', function() {
-    return gulp.src(config.path.src.article)
-        // .pipe(cache('article'))
-        .pipe(markdown())
-        .pipe(fileinclude({
-          prefix: '@@',
-          basepath: '@file'
-        }))
-        .on('data', function(file) {
-            // jsonfile.writeFile(config.files.cache, cache.caches, function (err) {})
-
-            var filePath    =   paths.basename(file.path);
-            // console.log(file.path)
-            var stats       =   fs.statSync(file.path);
-            var date        =   new Date(util.inspect(stats.mtime)).toLocaleString('en-US',config.date.options);
-            var contents    =   String(file.contents);
-            var title       =   /<h1>(.|\n)*<\/h1>/igm.exec(contents)[0];
-                title       =   title.replace('<h1>','').replace('</h1>','');
-
-            var tag         =   /<!--tags:(.*)-->/igm.exec(contents)[1];
-            var text        =   /<!--d-->(.|\n)*/igm.exec(contents)[0];
-            var href        =   "<a href='{0}'>{1}</a>"
-                                .replace('{0}',filePath)
-                                .replace('{1}',title);
-            contents        =   config.templates.article;
-            contents        =   contents.replace('{date}',date).replace('{href}',href).replace('{text}',text);
-            var html        =   fs.readFileSync(config.path.src.template+'article.html', 'utf8');
-            html            =   html.replace('{title}',title).replace('{text}',contents);
-            file.contents   =   new Buffer(html);
-            return file;
-        })
-        .pipe(fileinclude({
-          prefix: '@@',
-          basepath: '@file'
-        }))
-        .pipe(gulp.dest(config.path.build.article));
-});
-
-
-
-gulp.task('article:build', function() {
-    runSequence(['article:compile','article:rebase'],'html:build');
-    gulp.src(config.files.index)
-        // .pipe(rigger())
-        .pipe(fileinclude({
-          prefix: '@@',
-          basepath: '@file'
-        }))
-        .pipe(gulp.dest(config.path.build.html));      
-})
+gulp.task('auto-reload', function() {
+    var p;
+    gulp.watch('gulpfile.js', spawnChildren);
+    spawnChildren();
+    function spawnChildren(e) {
+        if (p) {p.kill()}
+        p = spawn('gulp', ['watch'], {stdio: 'inherit'}); 
+    } 
+}) 
 
 gulp.task('server', function() {
-  gulp.src(config.path.build.html)
-    .pipe(webserver(config.server)); 
-});
-
-gulp.task('reload-server', ['html:build','less:build','article:build'], function () {
-	gulp.src(config.path.build.html).pipe(refresh(webserver));
-	console.log('[sfedor-g] Сервер обновлён.');	
+    connect.server(config.server)
 })
 
+gulp.task('watch',function () {
+    gulp.watch([config.path.watch.templates.index],['index:build']);
+    gulp.watch([config.path.watch.templates.layout],['layout:build']);
+    gulp.watch([config.path.watch.templates.article],['article:build']);
+    gulp.watch([config.path.watch.templates.problems],['problems:build']);
+    gulp.watch([config.path.watch.style],['less:build']);
+    gulp.watch([config.path.watch.article], ['article:build']); 
+    // gulp.watch([config.path.watch.template, config.path.watch.html],['html:build']);    
+})
 
 gulp.task('html:build', function () {
     gulp.src(config.path.src.html) 
-        .on('data', function(file) {console.log(file.path)})
-        // .pipe(rigger())
-        .pipe(fileinclude({
-          prefix: '@@',
-          basepath: '@file'
-        }))
-        .pipe(gulp.dest(config.path.build.html));
+        .pipe(fileinclude(config.replace))
+        .pipe(gulp.dest(config.path.build.html))
+        .pipe(connect.reload());  
+})
+
+gulp.task('layout:build', function () {
+    runSequence(['article:build','article:rebase','html:build']);
 })
 
 
 gulp.task('less:build', function () {
-	gulp.src(['src/less/main.less'])
-		.pipe(less({plugins: [autoprefix]}))
-		// .pipe(concat('main.css')) 
-		.pipe(minifyCSS())   
-		.pipe(rename({suffix: ".min"}))
-		.pipe(gulp.dest(config.path.build.css));
+    gulp.src(['src/less/main.less'])
+        .pipe(less({plugins: [autoprefix]}))
+        // .pipe(concat('main.css')) 
+        .pipe(minifyCSS())   
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest(config.path.build.css))
+        .pipe(connect.reload());
 })
 
-
-gulp.task('default', ['server'], function() {
-	gulp.watch(
-		[	config.path.watch.html, 
-			config.path.watch.js, 
-			config.path.watch.fonts
-		], 
-		['reload-server']);
-	gulp.watch(
-		[config.path.watch.style],
-		['less:build']);
-
-    gulp.watch(
-        [config.path.watch.template],
-        ['html:build']);
-
-	gulp.watch(
-		[config.path.watch.article],
-		['article:build']);	
-	gulp.watch(
-		[config.path.watch.html],
-		['html:build']);	
+gulp.task('index:build', function() {
+    gulp.src(config.path.src.index)
+        .pipe(fileinclude(config.replace))
+        .pipe(gulp.dest(config.path.build.html))    
+        .pipe(connect.reload());  
 })
 
-// gulp.task('production',['html:build','article:build','less:build']);
+gulp.task('problems:build', function () {
+    gulp.src(config.path.src.problems) 
+        .pipe(fileinclude(config.replace))
+        .pipe(gulp.dest(config.path.build.html))
+        .pipe(connect.reload());  
+})
+
+gulp.task('article:build', function() {
+    runSequence(['article:rebase','index:build']);
+    return gulp.src(config.path.src.article)
+        // .pipe(cache('article'))
+        .pipe(markdown())
+        .pipe(fileinclude(config.replace))
+        .on('data',function(file){
+            jsonfile.writeFile(config.files.cache, cache.caches, function (err) {})
+            console.log('Скомпилирована статья -- ',paths.basename(file.path))
+            return build(file,"article");
+        })
+        .pipe(fileinclude(config.replace))
+        .pipe(gulp.dest(config.path.build.article))
+        .pipe(connect.reload()); 
+})
+
+gulp.task('article:rebase', function() {
+    return gulp.src(config.path.src.article)
+        .pipe(sort({comparator: function(file1,file2){return compare(file1,file2)}}))
+        .pipe(fileinclude(config.replace))
+        .pipe(markdown())
+        .on('data',function(file){
+            return build(file,"desc");
+        })
+        .pipe(fileinclude(config.replace))
+        .pipe(concat(config.files.articles))
+        .pipe(gulp.dest(config.path.src.template))
+})
