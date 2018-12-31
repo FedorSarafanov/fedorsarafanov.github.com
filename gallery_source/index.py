9e88644488ac87345abb20ac7bebb73674f1a452
Fedor Sarafanov <sfg180@yandex.ru>
2018-12-31 22:20:58 +0300

    Блог перенесен на движок Hugo

---
+++ gallery_source/index.py
@@ -0,0 +1,7 @@
+---
+title: "Исходники иллюстрации"
+type: "notpost"
+---
+<a class="imag2" href="/cook/gallery/{name}.tex"><img src="/cook/gallery/{name}.pdf.jpg" alt=""></a>
+<a href="/cook/gallery/pre">pre.tex</a>
+<pre><code class="language-latex">{code}</code></pre>
