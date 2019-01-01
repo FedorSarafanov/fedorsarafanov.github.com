7569cc39328f2c8e042407e528c7dc7e871f1a66
Fedor Sarafanov <sfg180@yandex.ru>
2019-01-01 13:07:06 +0300

    Исправлена подсветка кода в gallery

--- gallery_source/index.py
+++ gallery_source/index.py
@@ -1,7 +1,10 @@
 ---
 title: "Исходники иллюстрации"
 type: "notpost"
+date:  2019-01-01T13:04:49+03:00
 ---
 <a class="imag2" href="/cook/gallery/{name}.tex"><img src="/cook/gallery/{name}.pdf.jpg" alt=""></a>
 <a href="/cook/gallery/pre">pre.tex</a>
-<pre><code class="language-latex">{code}</code></pre>
+```tex
+{code}
+```
